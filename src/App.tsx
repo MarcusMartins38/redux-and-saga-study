a12b6a822ec433bb44faf392c19637cc5ea543b3
Marcus Martins <marcusmartins38@gmail.com>
2020-09-04 16:35:57 -0400

    Created Action, fixed the reducer

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import { Provider } from "react-redux";
 import store from "./store/index";
 
 import Catalog from "./components/Catalog";
+import Cart from "./components/Cart";
 
 function App() {
   return (
     <Provider store={store}>
       <Catalog />
+      <Cart />
     </Provider>
   );
 }
--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,15 +1,21 @@
-import React, { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
+import React, { useState, useEffect, useCallback } from "react";
+import { useDispatch } from "react-redux";
 import { IProduct } from "../store/modules/cart/types";
 import api from "../services/api";
+import { addProductToCart } from "../store/modules/cart/actions";
 
 const Catalog: React.FC = () => {
+  const dispatch = useDispatch();
   const [catalog, setCatalog] = useState<IProduct[]>([]);
 
   useEffect(() => {
     api.get("products").then((response) => {
       setCatalog(response.data);
     });
+  }, []);
+
+  const handleAddProductToCart = useCallback((product: IProduct) => {
+    dispatch(addProductToCart(product));
   }, []);
 
   return (
@@ -20,7 +26,9 @@ const Catalog: React.FC = () => {
         <article key={product.id}>
           <strong>{product.title}</strong> {"  -  "}
           <span>{product.price}</span> {"    "}
-          <button type="button">Comprar</button>
+          <button type="button" onClick={() => handleAddProductToCart(product)}>
+            Comprar
+          </button>
         </article>
       ))}
     </main>
